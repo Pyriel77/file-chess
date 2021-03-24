f2b0775136cbc03e99cfbf8c86787fabcdf27939
Pyriel77 <nyircsak.arpad@gmail.com>
2021-03-24 15:42:47 +0100

    all tests are green

--- src/main/java/com/codecool/chessopen/ChessResults.java
+++ src/main/java/com/codecool/chessopen/ChessResults.java
@@ -4,17 +4,58 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class ChessResults {
 
-    public List<String> getCompetitorsNamesFromFile(String fileName){
-        //TODO your code comes here
-        return null;
+    static class Competitor {
+        private String name;
+        private int points;
+        
+        public Competitor(String name, int points) {
+            this.name = name;
+            this.points = points;
+
+        }
+        public String getName() {
+            return name;
+        }
+        public double getPoints() {
+            return this.points;
+        }
     }
 
+
+    public List<String> getCompetitorsNamesFromFile(String fileName){
+
+        List<Competitor> competitors = new ArrayList<>();
+        List<String> names = new ArrayList<>();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            String line = "";
+            while (line != null) {
+                line = br.readLine();
+                if (line != null) {
+                    String[] pieces = line.split(",");
+                    competitors.add(new Competitor(pieces[0],
+                            (Integer.parseInt(pieces[1])
+                                    + Integer.parseInt(pieces[2])
+                                    + Integer.parseInt(pieces[3])
+                                    + Integer.parseInt(pieces[4])
+                                    + Integer.parseInt(pieces[5]))));
+                }
+            }
+            br.close();
+
+        } catch (IOException e) {
+            System.out.println("File not found!");
+        }
+        competitors.sort(Comparator.comparingDouble(Competitor::getPoints).reversed());
+
+        for (Competitor competitor : competitors) {
+            names.add(competitor.getName());
+        }
+        return names;
+    }
 }
